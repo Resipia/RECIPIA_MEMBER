ebb16d0e0597995f4c50e4852d788840642cf573
wlsdks <wlsdks12@naver.com>
2023-10-03 02:12:36 +0900

    RCP-17 fix: SRP(단일책임원칙)을 지키며 코드를 분리함

--- src/main/java/com/recipia/member/service/MemberService.java
+++ src/main/java/com/recipia/member/service/MemberService.java
@@ -7,6 +7,7 @@ import com.recipia.member.repository.MemberRepository;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.kafka.support.SendResult;
 import org.springframework.stereotype.Service;
 import org.springframework.util.concurrent.ListenableFuture;
 
@@ -48,16 +49,30 @@ public class MemberService {
     }
 
     /**
-     * 멤버 업데이트 이벤트를 Kafka에 전송하는 메서드
-     * 스프링3부터 ListenableFuture가 deprecated되어 CompletableFuture를 사용하면서 callback이 사라져서 whenComplete()로 콜백을 처리하도록 바뀜
+     * 멤버 업데이트 이벤트를 Kafka에 전송한다.
+     * KafkaTemplate의 send 메서드를 사용하여 메시지를 전송한 후, 결과를 handleKafkaResponse 메서드에 위임합니다.
+     * Spring 3 이후로 ListenableFuture가 deprecated 되어 CompletableFuture를 사용한다.
      */
     public CompletableFuture<Void> sendMemberUpdatedEvent(MemberDto memberDto) {
-        return kafkaTemplate.send("member-updated", memberDto.username())
-                .thenAccept(result -> log.debug("[kafka] member-updated 이벤트 발행, 업데이트한 유저 아이디 : {}", memberDto.username()))
-                .exceptionally(ex -> {
-                    log.error("Failed to send message: {}", ex.getMessage());
-                    return null;
-                });
+        CompletableFuture<SendResult<String, String>> future = kafkaTemplate.send("member-updated", memberDto.username());
+        return handleKafkaResponse(future, memberDto.username());
+    }
+
+    /**
+     * Kafka로 메시지를 성공적으로 보냈는지에 대한 결과를 처리한다.
+     * 로그를 남기거나 예외 상황을 처리하는 등의 후처리 작업을 담당한다.
+     *
+     * @param future 메시지 전송 결과를 담은 CompletableFuture 객체
+     * @param username 업데이트한 유저의 아이디
+     * @return 후처리 작업에 대한 CompletableFuture<Void> 객체
+     */
+    private CompletableFuture<Void> handleKafkaResponse(CompletableFuture<SendResult<String, String>> future, String username) {
+        return future.thenAccept(result -> {
+            log.debug("[kafka] member-updated 이벤트 발행, 업데이트한 유저 아이디 : {}", username);
+        }).exceptionally(ex -> {
+            log.error("Failed to send message: {}", ex.getMessage());
+            return null;
+        });
     }
 
 }
