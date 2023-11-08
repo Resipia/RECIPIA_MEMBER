15bd156d01511b7bfa34f9592e7bbdf62f8f1aa5
wlsdks <wlsdks12@naver.com>
2023-11-08 23:37:11 +0900

    RCP-35 chore: 코드 정리 실시
    1. 안쓰는 Kafka, Zipkin 관련코드 제거

--- src/main/java/com/recipia/member/config/aws/AwsSqsConfig.java
+++ src/main/java/com/recipia/member/config/aws/AwsSqsConfig.java
@@ -5,12 +5,9 @@ import lombok.Getter;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import software.amazon.awssdk.auth.credentials.AwsBasicCredentials;
 import software.amazon.awssdk.auth.credentials.AwsCredentials;
-import software.amazon.awssdk.auth.credentials.StaticCredentialsProvider;
 import software.amazon.awssdk.regions.Region;
 import software.amazon.awssdk.services.sqs.SqsAsyncClient;
-import software.amazon.awssdk.services.sqs.SqsClient;
 
 @Getter
 @Configuration
