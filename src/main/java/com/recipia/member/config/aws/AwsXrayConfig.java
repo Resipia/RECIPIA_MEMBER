2dd9886c8ca9c6045640d9d6441bbb8f06561a60
wlsdks <wlsdks12@naver.com>
2023-11-08 23:36:38 +0900

    RCP-35 feat: AWS X-ray Config 클래스 작성
    1. 이것도 이준이 누나가 전부 해놓은거 조금 손만봐서 수정함 의존성 javax -> jakarta로 변경 및 테스트 진행
    
    2. 필터 빈등록할때 반환값을 변경함 기존 코드에서는 필터 타입변환 에러가 발생했음

--- src/main/java/com/recipia/member/config/aws/AwsXrayConfig.java
+++ src/main/java/com/recipia/member/config/aws/AwsXrayConfig.java
@@ -1,23 +1,41 @@
 package com.recipia.member.config.aws;
 
+import com.amazonaws.xray.AWSXRay;
+import com.amazonaws.xray.AWSXRayRecorderBuilder;
+import com.amazonaws.xray.jakarta.servlet.AWSXRayServletFilter;
+import com.amazonaws.xray.plugins.EC2Plugin;
+import com.amazonaws.xray.plugins.ECSPlugin;
+import com.amazonaws.xray.strategy.sampling.LocalizedSamplingStrategy;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+
+import java.net.URL;
 
 @Configuration
 public class AwsXrayConfig {
 
-//    static {
-//        AWSXRayRecorderBuilder builder = AWSXRayRecorderBuilder.standard()
-//                .withPlugin(new EC2Plugin())
-//                .withPlugin(new ECSPlugin());
-//
-//        URL ruleFile = AwsXrayConfig.class.getResource("/sampling-rules.json");
-//        builder.withSamplingStrategy(new LocalizedSamplingStrategy(ruleFile));
-//
-//        AWSXRay.setGlobalRecorder(builder.build());
-//    }
-//
-//    @Bean
-//    public Filter TracingFilter() {
-//        return new AWSXRayServletFilter("springbootService");
-//    }
+    /**
+     * 이 코드는 AWS X-Ray SDK를 초기화한다.
+     * EC2 및 ECS 플러그인을 활성화하고 sampling-rules.json 파일에 정의된 샘플링 규칙을 사용한다.
+     */
+    static {
+        AWSXRayRecorderBuilder builder = AWSXRayRecorderBuilder.standard()
+                .withPlugin(new EC2Plugin())
+                .withPlugin(new ECSPlugin());
+
+        URL ruleFile = AwsXrayConfig.class.getResource("/sampling-rules.json");
+        builder.withSamplingStrategy(new LocalizedSamplingStrategy(ruleFile));
+
+        AWSXRay.setGlobalRecorder(builder.build());
+    }
+
+    /**
+     * 이 빈은 들어오는 HTTP 요청을 필터링하고 AWS X-Ray를 사용하여 추적하는 데 사용된다.
+     * 필터는 "springbootService"라는 이름을 사용하여 추적되는 서비스를 식별한다.
+     */
+    @Bean
+    public AWSXRayServletFilter TracingFilter() {
+        return new AWSXRayServletFilter("recipia-member");
+    }
+
 }
