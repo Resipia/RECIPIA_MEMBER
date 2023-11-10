c6f506473007254e31a7c7294290c5eeb938fd3d
yijoon009 <yijoon009@gmail.com>
2023-11-10 10:32:23 +0900

    main 병합 충돌 해결

--- src/main/java/com/recipia/member/config/aws/AwsXrayConfig.java
+++ src/main/java/com/recipia/member/config/aws/AwsXrayConfig.java
@@ -18,7 +18,15 @@ public class AwsXrayConfig {
      * 이 코드는 AWS X-Ray SDK를 초기화한다.
      * EC2 및 ECS 플러그인을 활성화하고 sampling-rules.json 파일에 정의된 샘플링 규칙을 사용한다.
      */
+    // static 블록은 클래스가 JVM에 로드될 때 단 한 번 실행된다. 이곳에서 AWS X-Ray 레코더를 설정하고 초기화한다.
     static {
+
+        /**
+         * AWSXRayRecorderBuilder는 AWS X-Ray 레코더 인스턴스를 구성하기 위한 빌더 패턴 클래스다.
+         * standard() 메소드는 기본 구성으로 빌더를 생성한다.
+         * withPlugin 메소드를 사용하여 EC2 및 ECS 플러그인을 레코더에 추가한다.
+         * 이 플러그인들은 AWS X-Ray가 EC2 인스턴스와 ECS 컨테이너에서 실행 중인 애플리케이션에 대한 추가적인 정보를 수집하도록 한다.
+         */
         AWSXRayRecorderBuilder builder = AWSXRayRecorderBuilder.standard()
                 .withPlugin(new EC2Plugin())
                 .withPlugin(new ECSPlugin());
