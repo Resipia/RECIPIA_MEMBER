7d47d39a8623a6726786580be923b17f5cd0e3d4
yijoon009 <yijoon009@gmail.com>
2023-11-16 14:19:27 +0900

    RCP-56 feat: SNS, SQS 로직에서 zipkin관련 Trace 제거

--- src/main/java/com/recipia/member/aws/SqsListenerService.java
+++ src/main/java/com/recipia/member/aws/SqsListenerService.java
@@ -1,7 +1,5 @@
 package com.recipia.member.aws;
 
-import brave.Span;
-import brave.Tracer;
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.ObjectMapper;
@@ -16,7 +14,6 @@ import org.springframework.stereotype.Service;
 public class SqsListenerService {
 
     private final ObjectMapper objectMapper;
-    private final Tracer tracer;
 
     @SqsListener(value = "${spring.cloud.aws.sqs.nickname-sqs-name}")
     public void receiveMessage(String messageJson) throws JsonProcessingException {
@@ -24,23 +21,14 @@ public class SqsListenerService {
         JsonNode messageNode = objectMapper.readTree(messageJson);
         String messageId = messageNode.get("MessageId").asText();  // 메시지 ID 추출
 
-        Span newSpan = tracer.nextSpan().name(messageId).start(); // Span 이름을 메시지 ID로 설정
-        newSpan.tag("messageId", messageId); // messageId 태그 추가
-        newSpan.tag("consumer", "MEMBER"); // consumer 태그 추가
+        // SQS 메시지 처리 로직
+        String messageContent = messageNode.get("Message").asText();
 
-        try (Tracer.SpanInScope ws = tracer.withSpanInScope(newSpan)) {
-            // SQS 메시지 처리 로직
-            String topicArn = messageNode.get("TopicArn").asText();
-            String messageContent = messageNode.get("Message").asText();
+        log.info("[MEMBER] Received message from SQS with messageId: {}", messageId);
 
-            log.info("[MEMBER] Received message from SQS with messageId: {}", messageId);
-
-            // Assuming the "Message" is also a JSON string, we parse it to print as JSON object
-            JsonNode message = objectMapper.readTree(messageContent);
-            log.info("Message:  {}", message.toString());
-        } finally {
-            newSpan.finish();
-        }
+        // Assuming the "Message" is also a JSON string, we parse it to print as JSON object
+        JsonNode message = objectMapper.readTree(messageContent);
+        log.info("Message:  {}", message.toString());
 
     }
 
