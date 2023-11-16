cf0d671f988a4babe61341508eff53bc0f3a6a50
yijoon009 <yijoon009@gmail.com>
2023-11-16 17:30:28 +0900

    RCP-57 feat: span name "feign get nickname"로 설정

--- src/main/java/com/recipia/member/feign/controller/MemberFeignController.java
+++ src/main/java/com/recipia/member/feign/controller/MemberFeignController.java
@@ -1,5 +1,7 @@
 package com.recipia.member.feign.controller;
 
+import brave.Span;
+import brave.Tracer;
 import com.recipia.member.feign.dto.NicknameDto;
 import com.recipia.member.feign.service.MemberFeignService;
 import lombok.RequiredArgsConstructor;
@@ -14,10 +16,16 @@ import org.springframework.web.bind.annotation.RestController;
 public class MemberFeignController {
 
     private final MemberFeignService memberFeignService;
+    private final Tracer tracer;
 
     @PostMapping("/getNickname")
     public NicknameDto getNickname(@RequestParam(name = "memberId") Long memberId) {
-        return memberFeignService.getNicknameByMemberId(memberId);
+        Span span = tracer.nextSpan().name("Feign Get Nickname").start();
+        try (Tracer.SpanInScope ws = tracer.withSpanInScope(span)) {
+            return memberFeignService.getNicknameByMemberId(memberId);
+        } finally {
+            span.finish();
+        }
     }
 
 }
