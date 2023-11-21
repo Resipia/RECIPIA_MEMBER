1f390dc0611c53a2cbb42e48e5f9f3398eb263c5
yijoon009 <yijoon009@gmail.com>
2023-11-21 15:17:52 +0900

    RCP-160 refactor: SNS 발행 메소드 호출 전 완전한 메세지 생성 (traceid 포함)

--- src/main/java/com/recipia/member/springevent/SpringEventSnsPublishListener.java
+++ src/main/java/com/recipia/member/springevent/SpringEventSnsPublishListener.java
@@ -1,5 +1,6 @@
 package com.recipia.member.springevent;
 
+import brave.Tracer;
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.recipia.member.aws.SnsService;
 import com.recipia.member.utils.CustomJsonBuilder;
@@ -16,6 +17,7 @@ public class SpringEventSnsPublishListener {
 
     private final SnsService snsService;
     private final CustomJsonBuilder customJsonBuilder;
+    private final Tracer tracer;
 
     /**
      * 이벤트를 호출한 서비스 코드의 트랜잭션과 묶여있지 않고 트랜잭션이 commit된 후에 동작한다.
@@ -23,9 +25,13 @@ public class SpringEventSnsPublishListener {
     @TransactionalEventListener(phase = TransactionPhase.AFTER_COMMIT)
     public void snsListen(NicknameChangeSpringEvent event) throws JsonProcessingException {
 
-        // message에 memberId 주입
+        // 현재 TraceID 추출
+        String traceId = tracer.currentSpan().context().traceIdString();
+
+        // message에 memberId, traceId 주입
         String messageJson = customJsonBuilder
                 .add("memberId", event.memberId().toString())
+                .add("traceId", traceId)
                 .build();
 
         snsService.publishNicknameToTopic(messageJson);
