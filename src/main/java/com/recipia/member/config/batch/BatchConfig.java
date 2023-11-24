5708267c9479a414c0f254e90a379017098446fe
wlsdks <wlsdks12@naver.com>
2023-11-24 23:47:49 +0900

    RCP-179 refactor: isBatch 테이블을 변경하던 로직을 제거하고 TraceId를 추가해주던 로직도 제거함

--- src/main/java/com/recipia/member/config/batch/BatchConfig.java
+++ src/main/java/com/recipia/member/config/batch/BatchConfig.java
@@ -1,7 +1,5 @@
 package com.recipia.member.config.batch;
 
-import brave.Span;
-import brave.Tracer;
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.databind.node.ObjectNode;
@@ -34,12 +32,11 @@ import org.springframework.transaction.annotation.Transactional;
 @Slf4j
 @RequiredArgsConstructor
 @Configuration
-public class BatchConfiguration extends DefaultBatchConfiguration {
+public class BatchConfig extends DefaultBatchConfiguration {
 
     private final SnsService snsService;
     private final EntityManagerFactory entityManagerFactory;
     private final ObjectMapper objectMapper;
-    private final Tracer tracer;
 
 
     // 배치 작업에서 사용할 chunkSize 값 설정. 기본값은 1000
@@ -114,118 +111,26 @@ public class BatchConfiguration extends DefaultBatchConfiguration {
      */
     @Bean
     public ItemProcessor<MemberEventRecord, MemberEventRecord> itemProcessor() {
-        log.info("ItemProcessor 동작!");
-
         return item -> {
             try {
-                processItem(item);
+                log.info("processItem 동작중");
+
+                // 1. 이벤트 객체의 attribute를 memberid, traceid 를 지닌 메시지 dto로 변환
+                SnsMessageDto snsMessageDto = objectMapper.readValue(item.getAttribute(), SnsMessageDto.class);
+
+                // 새로운 JSON 메시지 생성
+                String newJsonMessage = createJsonMessage(snsMessageDto);
+
+                // 4. SNS 발행 메소드
+                snsService.publishNicknameToTopic(newJsonMessage);
+
             } catch (JsonProcessingException e) {
                 log.error("JSON 처리 중 오류 발생: {}", e.getMessage(), e);
-                // 예외 처리 로직 추가 (필요에 따라)
             }
             return item;
         };
 
     }
-
-
-    private void processItem(MemberEventRecord item) throws JsonProcessingException {
-
-        log.info("processItem 동작중");
-
-        // publishNicknameToTopic 메소드에 전달할 메시지 변수 선언
-        String newJsonMessage = "";
-
-        // 1. 이벤트 객체의 attribute를 memberid, traceid 를 지닌 메시지 dto로 변환
-        SnsMessageDto snsMessageDto = objectMapper.readValue(item.getAttribute(), SnsMessageDto.class);
-
-        // 2. snsMessageDto에 traceid 있는지 확인
-        if (snsMessageDto.traceId() != null) {
-            // 2-1. traceid 있으면 바로 sns publish
-            // attribute JSON 파싱 및 새로운 JSON 문자열 생성
-            newJsonMessage = createJsonMessage(snsMessageDto);
-        } else {
-            // 2-2. traceid 없으면 새로 생성해주고 메시지에 담아서 sns publish
-
-            // 새로운 Span 생성
-            Span newSpan = tracer.newTrace();
-            // 새로운 traceId 추출
-            String newTraceId = newSpan.context().traceIdString();
-
-            SnsMessageDto newSnsMessageDto = SnsMessageDto.of(newTraceId, snsMessageDto.memberId());
-
-
-            // 새로운 JSON 메시지 생성
-            newJsonMessage = createJsonMessage(newSnsMessageDto);
-
-            // Span 종료
-            newSpan.finish();
-
-            // 2-3. 새로 생성된 traceid를 추가해서 attribute를 업데이트
-            item.changeAttribute(newJsonMessage);
-
-        }
-
-        // 3. 배치로 재발행된 이벤트는 is_batch = true로 업데이트
-        item.changeIsBatchTrue();
-
-        // 4. SNS 발행 메소드
-        snsService.publishNicknameToTopic(newJsonMessage);
-    }
-
-    ;
-
-//        // 1. 이벤트 객체의 attribute를 memberid, traceid 를 가진 메시지 dto로 변환
-//        SnsMessageDto snsMessageDto = objectMapper.readValue(item.getAttribute(), SnsMessageDto.class);
-//
-//        // 2. snsMessageDto에 traceid 있는지 확인
-//        if (snsMessageDto.traceId() == null) {
-//            // 2-1. traceid 없으면 새로 생성해주고 메시지에 담아서 sns publish
-//
-//            // 새로운 Span 생성
-//            Span newSpan = tracer.newTrace();
-//            // 새로운 traceId 추출
-//            String newTraceId = newSpan.context().traceIdString();
-//            // 새로운 JSON 객체 생성
-//            ObjectNode jsonNode = objectMapper.createObjectNode();
-//            jsonNode.put(TRACE_ID, newTraceId);
-//            jsonNode.put(MEMBER_ID, item.getMember().getId().toString());
-//            // 새로 생성된 traceid를 추가해서 attribute를 업데이트
-//            item.changeAttribute(objectMapper.writeValueAsString(jsonNode));
-//            // 새로운 Span 종료
-//            newSpan.finish();
-//        }
-//
-//        // attribute JSON 파싱 및 새로운 JSON 문자열 생성
-//        String newJsonMessage = createJsonMessage(item.getAttribute());
-//
-//        // is_batch = true로 업데이트
-//        item.changeIsBatchTrue();
-//
-//        // SNS 발행 메소드
-//        snsService.publishNicknameToTopic(newJsonMessage);
-
-
-    /**
-     * [EXTRACT METHOD] - DB안의 데이터를 다시 parsing해서 뽑아낸 다음 그대로 다시 새로운 json으로 만드는 메서드
-     * <p>
-     * 이렇게 다시 가공을 안하고 바로 이벤트 저장소 테이블에 저장된 attribute 값을 파라미터로 넣어서 SNS를 발행했더니
-     * SqsListener에서 메시지를 꺼내서 사용할 때 JSON PARSING 에러가 발생해서 이렇게 json 재가공을 해준것이다.
-     */
-//    private String createJsonMessage(String json) throws JsonProcessingException {
-//        ObjectNode jsonNode = (ObjectNode) objectMapper.readTree(json);
-//        ObjectNode newJsonNode = objectMapper.createObjectNode();
-//        newJsonNode.put(TRACE_ID, jsonNode.get(TRACE_ID).asText());
-//        newJsonNode.put(MEMBER_ID, jsonNode.get(MEMBER_ID).asText());
-//        return newJsonNode.toString();
-//    }
-    private String createJsonMessage(SnsMessageDto snsMessageDto) throws JsonProcessingException {
-        ObjectNode newJsonNode = objectMapper.createObjectNode();
-        newJsonNode.put(TRACE_ID, snsMessageDto.traceId());
-        newJsonNode.put(MEMBER_ID, snsMessageDto.memberId());
-        return newJsonNode.toString();
-    }
-
 
     /**
      * ItemWriter를 설정. 업데이트된 엔터티를 처리. 여기서 작업이 마무리되면 트랜잭션이 종료되고 커밋된다.
@@ -237,5 +142,17 @@ public class BatchConfiguration extends DefaultBatchConfiguration {
         writer.setEntityManagerFactory(entityManagerFactory);
         return writer;
     }
+
+    /**
+     * [EXTRACT METHOD] - DB안의 데이터를 다시 parsing해서 뽑아낸 다음 그대로 다시 새로운 json으로 만드는 메서드
+     */
+    private String createJsonMessage(SnsMessageDto snsMessageDto) throws JsonProcessingException {
+        ObjectNode newJsonNode = objectMapper.createObjectNode();
+        newJsonNode.put(TRACE_ID, snsMessageDto.traceId());
+        newJsonNode.put(MEMBER_ID, snsMessageDto.memberId().toString());
+        return newJsonNode.toString();
+    }
+
+
 
 }
