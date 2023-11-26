e813cbcd1743156f6d6a14a52d27bfcddc914bf4
yijoon009 <yijoon009@gmail.com>
2023-11-26 17:27:22 +0900

    RCP-195 fix: sns 메시지에서 traceid 제거

--- src/main/java/com/recipia/member/dto/SnsMessageDto.java
+++ src/main/java/com/recipia/member/dto/SnsMessageDto.java
@@ -1,9 +1,4 @@
 package com.recipia.member.dto;
 
-public record SnsMessageDto(String traceId, Long memberId) {
-
-
-    public static SnsMessageDto of(String traceId, Long memberId) {
-        return new SnsMessageDto(traceId, memberId);
-    }
+public record SnsMessageDto(Long memberId) {
 }
