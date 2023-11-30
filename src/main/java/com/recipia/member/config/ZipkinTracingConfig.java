257e4856c5357cfbf21a4dc634fb44e4a91f143c
yijoon009 <yijoon009@gmail.com>
2023-11-30 18:05:18 +0900

    zipkin 추적 설정 변경

--- src/main/java/com/recipia/member/config/ZipkinTracingConfig.java
+++ src/main/java/com/recipia/member/config/ZipkinTracingConfig.java
@@ -14,27 +14,27 @@ import java.util.List;
 @Configuration
 public class ZipkinTracingConfig {
 
-    private static final List<String> ALLOWED_PATHS = Arrays.asList(
-            "/member/nicknameChange"    // 허용할 첫 번째 URL 패턴
-//            "/api/another",    // 허용할 두 번째 URL 패턴
-//            "/api/more"        // 추가로 허용할 URL 패턴
-    );
-
-    @Bean
-    public Sampler customSampler() {
-        return new Sampler() {
-            @Override
-            public boolean isSampled(long traceId) {
-                RequestAttributes requestAttributes = RequestContextHolder.getRequestAttributes();
-
-                if (requestAttributes instanceof ServletRequestAttributes) {
-                    HttpServletRequest request = ((ServletRequestAttributes) requestAttributes).getRequest();
-                    return ALLOWED_PATHS.stream().anyMatch(path -> request.getRequestURI().startsWith(path));
-                }
-
-                // HTTP 요청 정보가 없는 경우 기본적으로 추적하지 않음
-                return false;
-            }
-        };
-    }
+//    private static final List<String> ALLOWED_PATHS = Arrays.asList(
+//            "/member/nicknameChange"    // 허용할 첫 번째 URL 패턴
+////            "/api/another",    // 허용할 두 번째 URL 패턴
+////            "/api/more"        // 추가로 허용할 URL 패턴
+//    );
+//
+//    @Bean
+//    public Sampler customSampler() {
+//        return new Sampler() {
+//            @Override
+//            public boolean isSampled(long traceId) {
+//                RequestAttributes requestAttributes = RequestContextHolder.getRequestAttributes();
+//
+//                if (requestAttributes instanceof ServletRequestAttributes) {
+//                    HttpServletRequest request = ((ServletRequestAttributes) requestAttributes).getRequest();
+//                    return ALLOWED_PATHS.stream().anyMatch(path -> request.getRequestURI().startsWith(path));
+//                }
+//
+//                // HTTP 요청 정보가 없는 경우 기본적으로 추적하지 않음
+//                return false;
+//            }
+//        };
+//    }
 }
