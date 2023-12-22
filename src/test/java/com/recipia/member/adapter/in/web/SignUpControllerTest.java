22899d0156b7a9b52c95852b72a89ab295e21a2f
yijoon009 <yijoon009@gmail.com>
2023-12-22 23:42:37 +0900

    RCP-240 test: 회원가입 필수값 누락 테스트 완료

--- src/test/java/com/recipia/member/adapter/in/web/SignUpControllerTest.java
+++ src/test/java/com/recipia/member/adapter/in/web/SignUpControllerTest.java
@@ -2,19 +2,16 @@ package com.recipia.member.adapter.in.web;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.recipia.member.adapter.in.web.dto.SignUpRequestDto;
-import com.recipia.member.config.TestSecurityConfig;
-import com.recipia.member.config.TotalTestSupport;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.boot.test.mock.mockito.MockBean;
-import org.springframework.context.annotation.Import;
 import org.springframework.http.MediaType;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.web.servlet.MockMvc;
 
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
@@ -49,6 +46,29 @@ class SignUpControllerTest{
                 .andDo(print());
     }
 
+
+    @Test
+    @DisplayName("[fail] 필수 입력값이 누락된 dto가 들어왔을 때 BadRequest를 반환한다")
+    void testMissingRequiredFields() throws Exception {
+        //given
+        SignUpRequestDto invalidRequest = SignUpRequestDto.of(
+                null, "password123", null, "johndoe",
+                "Hello, I'm John", "010-1234-5678", "123 Main St", "Apt 101",
+                "Y", "Y", "Y", "Y"
+        );
+
+        //when & then
+        mockMvc.perform(post("/member/signUp")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(asJsonString(invalidRequest)))
+                .andExpect(status().isBadRequest())
+                .andDo(print())
+                .andExpect(result -> {
+                    String responseString = result.getResponse().getContentAsString();
+                    assertThat(responseString).contains("email");
+                    assertThat(responseString).contains("fullName");
+                });
+        }
 
     // JSON 문자열 변환을 위한 유틸리티 메서드
     private String asJsonString(final Object obj) {
