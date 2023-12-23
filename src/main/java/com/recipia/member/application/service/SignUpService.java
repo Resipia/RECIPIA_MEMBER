88aec20dfb5642ef5b6dbde63b7bd94019a3a338
yijoon009 <yijoon009@gmail.com>
2023-12-23 14:50:21 +0900

    RCP-240 feat: 회원가입 서비스 완성

--- src/main/java/com/recipia/member/application/service/SignUpService.java
+++ src/main/java/com/recipia/member/application/service/SignUpService.java
@@ -2,6 +2,8 @@ package com.recipia.member.application.service;
 
 import com.recipia.member.application.port.in.SignUpUseCase;
 import com.recipia.member.application.port.out.port.SignUpPort;
+import com.recipia.member.common.exception.ErrorCode;
+import com.recipia.member.common.exception.MemberApplicationException;
 import com.recipia.member.domain.Member;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
@@ -26,6 +28,14 @@ public class SignUpService implements SignUpUseCase {
 
     @Override
     public Long signUp(Member member) {
+        // 이메일 형태 및 비밀번호 형태 검증
+        if (!Member.isValidEmail(member.getEmail()) || !Member.isValidPassword(member.getPassword())) {
+            throw new MemberApplicationException(ErrorCode.BAD_REQUEST);
+        }
+
+        // 비밀번호 암호화
+        Member.passwordEncoder(member);
+
         return signUpPort.signUpMember(member);
     }
 
