8913f9a091f2d9f823c362e2ffc0805317d355f7
yijoon009 <yijoon009@gmail.com>
2023-12-23 14:50:08 +0900

    RCP-240 feat: 회원가입 컨트롤러 완성

--- src/main/java/com/recipia/member/adapter/in/web/SignUpController.java
+++ src/main/java/com/recipia/member/adapter/in/web/SignUpController.java
@@ -2,22 +2,28 @@ package com.recipia.member.adapter.in.web;
 
 import com.recipia.member.adapter.in.web.dto.SignUpRequestDto;
 import com.recipia.member.adapter.in.web.dto.response.ResponseDto;
+import com.recipia.member.application.port.in.SignUpUseCase;
+import com.recipia.member.domain.converter.MemberConverter;
 import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+@RequiredArgsConstructor
 @RequestMapping("/member")
 @RestController
 public class SignUpController {
 
+    private final SignUpUseCase signUpUseCase;
+
     @PostMapping("/signUp")
     public ResponseEntity<ResponseDto<Long>> signUp(@Valid @RequestBody SignUpRequestDto requestDto) {
-
+        Long memberId = signUpUseCase.signUp(MemberConverter.requestToDomain(requestDto));
         return ResponseEntity.ok(
-                ResponseDto.success(1L)
+                ResponseDto.success(memberId)
         );
     }
 
