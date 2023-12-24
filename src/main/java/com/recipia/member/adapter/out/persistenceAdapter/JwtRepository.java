cc1b9e90ccd964094328d79eba17011b8734b495
yijoon009 <yijoon009@gmail.com>
2023-12-24 23:06:42 +0900

    RCP-237 feat: jwt repo 임시 커밋

--- src/main/java/com/recipia/member/adapter/out/persistenceAdapter/JwtRepository.java
+++ src/main/java/com/recipia/member/adapter/out/persistenceAdapter/JwtRepository.java
@@ -4,6 +4,9 @@ import com.recipia.member.adapter.out.persistence.JwtEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
 public interface JwtRepository extends JpaRepository<JwtEntity, Long> {
+    Optional<JwtEntity> findJwtByMemberIdAndRefreshToken(Long memberId, String refreshToken);
 }
