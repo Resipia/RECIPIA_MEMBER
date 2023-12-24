a589837dc6fb88517de0e10c5f47199a34ffbdc1
yijoon009 <yijoon009@gmail.com>
2023-12-24 17:40:50 +0900

    RCP-237 feat: jwtAdapter, jwtPort 완성

--- src/main/java/com/recipia/member/adapter/out/persistenceAdapter/JwtAdapter.java
+++ src/main/java/com/recipia/member/adapter/out/persistenceAdapter/JwtAdapter.java
@@ -1,10 +1,15 @@
 package com.recipia.member.adapter.out.persistenceAdapter;
 
+import com.recipia.member.adapter.out.persistence.JwtEntity;
 import com.recipia.member.application.port.out.port.JwtPort;
+import com.recipia.member.common.exception.ErrorCode;
+import com.recipia.member.common.exception.MemberApplicationException;
 import com.recipia.member.domain.Jwt;
 import com.recipia.member.domain.converter.JwtConverter;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Component;
+
+import java.util.Optional;
 
 @RequiredArgsConstructor
 @Component
@@ -16,4 +21,14 @@ public class JwtAdapter implements JwtPort {
     public void save(Jwt jwt) {
         jwtRepository.save(JwtConverter.domainToEntity(jwt));
     }
+
+    /**
+     * jwt의 memberid, refreshToken으로 Jwt 가져오기
+     */
+    @Override
+    public Jwt getJwt(Jwt jwt) {
+        JwtEntity jwtEntity = jwtRepository.findJwtByMemberIdAndRefreshToken(jwt.getMemberId(), jwt.getRefreshToken()).orElseThrow(() -> new MemberApplicationException(ErrorCode.JWT_NOT_FOUND));
+        return JwtConverter.entityToDomain(jwtEntity);
+    }
+
 }
