9293050b8e16233cea7b5d604d0006f5acf64a6b
yijoon009 <yijoon009@gmail.com>
2023-12-25 12:05:20 +0900

    RCP-237 refactor: DB 관련 에러는 어댑터에서 관리하도록 로직 수정

--- src/main/java/com/recipia/member/adapter/out/persistenceAdapter/MemberAdapter.java
+++ src/main/java/com/recipia/member/adapter/out/persistenceAdapter/MemberAdapter.java
@@ -3,6 +3,8 @@ package com.recipia.member.adapter.out.persistenceAdapter;
 import com.recipia.member.adapter.out.persistence.MemberEntity;
 import com.recipia.member.adapter.out.persistence.constant.MemberStatus;
 import com.recipia.member.application.port.out.port.MemberPort;
+import com.recipia.member.common.exception.ErrorCode;
+import com.recipia.member.common.exception.MemberApplicationException;
 import com.recipia.member.domain.Member;
 import com.recipia.member.domain.converter.MemberConverter;
 import lombok.RequiredArgsConstructor;
@@ -17,17 +19,28 @@ public class MemberAdapter implements MemberPort {
     private final MemberRepository memberRepository;
 
     @Override
-    public Optional<Member> findMemberById(Long memberId) {
-        Optional<MemberEntity> memberEntityOptional = memberRepository.findMemberById(memberId);
-        return memberEntityOptional.map(MemberConverter::entityToDomain);
+    public Member findMemberById(Long memberId) {
+        MemberEntity memberEntityOptional = memberRepository.findMemberById(memberId).orElseThrow(() -> new MemberApplicationException(ErrorCode.USER_NOT_FOUND));
+        return MemberConverter.entityToDomain(memberEntityOptional);
     }
 
+    /**
+     * 회원가입에서도 사용되기 때문에 일부러 어기서 exception 발생시키지 않음
+     * 회원가입 할때 사용자가 입력한 이메일이 db에 존재하지 않으면 true를 반환해야하기 때문
+     */
     @Override
     public Optional<Member> findMemberByEmail(String email) {
         Optional<MemberEntity> memberEntityOptional = memberRepository.findMemberByEmail(email);
         return memberEntityOptional.map(MemberConverter::entityToDomain);
     }
 
+    /**
+     * 공통 클래스인 TokenValidator 에서 반환받은 member이 null인지 아닌지에 따라 boolean 값 반환하는데 사용되기때문에
+     * 여기서 memberEntity 없을때 exception 던지지 않음
+     * @param email
+     * @param status
+     * @return
+     */
     @Override
     public Optional<Member> findMemberByEmailAndStatus(String email, MemberStatus status) {
         Optional<MemberEntity> memberEntityOptional = memberRepository.findMemberByEmailAndStatus(email, status);
