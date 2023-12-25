913a7eec4de1eac34deb3966aa8750c7e0382661
yijoon009 <yijoon009@gmail.com>
2023-12-25 15:39:09 +0900

    RCP-237 feat: jwt 재발행 서비스 레이어 완성

--- src/main/java/com/recipia/member/application/port/in/JwtUseCase.java
+++ src/main/java/com/recipia/member/application/port/in/JwtUseCase.java
@@ -1,9 +1,11 @@
 package com.recipia.member.application.port.in;
 
+import com.recipia.member.domain.Jwt;
 import org.springframework.data.util.Pair;
 
 import java.time.LocalDateTime;
 
 public interface JwtUseCase {
     void insertRefreshTokenToDB(String email, Pair<String, LocalDateTime> jwtPair);
+    String republishAccessToken(Jwt jwt);
 }
