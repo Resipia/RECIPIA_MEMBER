d71308d814434d6715b214cfcd93eb36f526c2ba
yijoon009 <yijoon009@gmail.com>
2023-12-29 03:14:09 +0900

    [RCP-242] : :white_check_mark: 휴대폰 번호 관련 Authentication 도메인 테스트

---
+++ src/test/java/com/recipia/member/domain/AuthenticationTest.java
@@ -0,0 +1,33 @@
+package com.recipia.member.domain;
+
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.*;
+
+class AuthenticationTest {
+
+    @DisplayName("[happy] 사용자가 입력한 전화번호를 +8201000000000 형태로 변환한다.")
+    @Test
+    void formatPhoneNumberSuccess() {
+        //given
+        Authentication authentication = createAuthentication();
+        String beforePhoneNumber = authentication.getPhoneNumber();
+        String afterPhoneNumber = "+8201000001111";
+
+        //when
+        authentication.formatPhoneNumber();
+
+        //then
+        assertThat(beforePhoneNumber).isNotEqualTo(authentication.getPhoneNumber());
+        assertEquals(authentication.getPhoneNumber(), afterPhoneNumber);
+
+    }
+
+    private static Authentication createAuthentication() {
+        return Authentication.of("        010-0000-1111   ", null);
+    }
+
+}
