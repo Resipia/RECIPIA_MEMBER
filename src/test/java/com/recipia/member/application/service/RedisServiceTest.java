89111f10024ba71117553996556bf080a25b73e2
yijoon009 <yijoon009@gmail.com>
2023-12-30 02:01:40 +0900

    [RCP-242] : :white_check_mark: 레디스 서비스 테스트, 테스트용 데이터베이스 연동

---
+++ src/test/java/com/recipia/member/application/service/RedisServiceTest.java
@@ -0,0 +1,86 @@
+package com.recipia.member.application.service;
+
+import com.recipia.member.config.TotalTestSupport;
+import org.assertj.core.api.Assertions;
+import org.awaitility.Awaitility;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import java.time.Duration;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+// fixme: 단위로 할지, 통합으로 할지 결정
+class RedisServiceTest extends TotalTestSupport {
+
+    final String KEY = "key";
+    final String VALUE = "value";
+    final Duration DURATION = Duration.ofMillis(5000);
+
+    @Autowired
+    private RedisService redisService;
+
+    @BeforeEach
+    void shutDown() {
+        redisService.setValues(KEY, VALUE, DURATION);
+    }
+
+    @AfterEach
+    void tearDown() {
+        redisService.deleteValues(KEY);
+    }
+
+    @Test
+    @DisplayName("[happy] Redis에 데이터를 저장하면 정상적으로 조회된다.")
+    void saveAndFindTest() throws Exception {
+        // when
+        String findValue = redisService.getValues(KEY);
+
+        // then
+        assertThat(VALUE).isEqualTo(findValue);
+    }
+
+    @Test
+    @DisplayName("[happy] Redis에 저장된 데이터를 수정할 수 있다.")
+    void updateTest() throws Exception {
+        // given
+        String updateValue = "updateValue";
+        redisService.setValues(KEY, updateValue, DURATION);
+
+        // when
+        String findValue = redisService.getValues(KEY);
+
+        // then
+        assertThat(updateValue).isEqualTo(findValue);
+        assertThat(VALUE).isNotEqualTo(findValue);
+    }
+
+    @Test
+    @DisplayName("[happy] Redis에 저장된 데이터를 삭제할 수 있다.")
+    void deleteTest() throws Exception {
+        // when
+        redisService.deleteValues(KEY);
+        String findValue = redisService.getValues(KEY);
+
+        // then
+        assertThat(findValue).isEqualTo("false");
+    }
+
+    @Test
+    @DisplayName("[happy] Redis에 저장된 데이터는 만료시간이 지나면 삭제된다.")
+    void expiredTest() throws Exception {
+        // when
+        String findValue = redisService.getValues(KEY);
+        Awaitility.await().pollDelay(Duration.ofMillis(6000)).untilAsserted(
+                () -> {
+                    String expiredValue = redisService.getValues(KEY);
+                    assertThat(expiredValue).isNotEqualTo(findValue);
+                    assertThat(expiredValue).isEqualTo("false");
+                }
+        );
+    }
+
+}
