48f1d975ff7ee2313a6260e7f5965e5321d403dc
yijoon009 <yijoon009@gmail.com>
2024-01-02 16:04:09 +0900

    [RCP-] :sparkles: 인증코드 검증 컨트롤러 로직 구현

--- src/main/java/com/recipia/member/adapter/in/web/AuthController.java
+++ src/main/java/com/recipia/member/adapter/in/web/AuthController.java
@@ -1,15 +1,17 @@
 package com.recipia.member.adapter.in.web;
 
+import com.recipia.member.adapter.in.web.dto.request.CheckVerifyCodeRequestDto;
 import com.recipia.member.adapter.in.web.dto.request.PhoneNumberRequestDto;
 import com.recipia.member.adapter.in.web.dto.response.ResponseDto;
-import com.recipia.member.adapter.out.aws.TokyoSnsService;
 import com.recipia.member.application.port.in.AuthUseCase;
-import com.recipia.member.domain.Authentication;
 import com.recipia.member.domain.converter.AuthConverter;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @RequestMapping("/member/auth")
 @RequiredArgsConstructor
@@ -17,12 +19,21 @@ import org.springframework.web.bind.annotation.*;
 public class AuthController {
 
     private final AuthUseCase authUseCase;
+    private final AuthConverter authConverter;
 
     @PostMapping("/phone")
     public ResponseEntity<ResponseDto<Void>> sendPhoneNumber(@Valid @RequestBody PhoneNumberRequestDto requestDto) {
-        authUseCase.verityPhoneNumber(AuthConverter.requestToDomain(requestDto));
+        authUseCase.verifyPhoneNumber(authConverter.phoneNumberRequestDtoToDomain(requestDto));
         return ResponseEntity.ok(
                 ResponseDto.success()
+        );
+    }
+
+    @PostMapping("/check/verifyCode")
+    public ResponseEntity<ResponseDto<Boolean>> checkVerifyCode(@Valid @RequestBody CheckVerifyCodeRequestDto requestDto) {
+        boolean isValidCode = authUseCase.checkVerifyCode(authConverter.checkVerifyCodeRequestDtoToDomain(requestDto));
+        return ResponseEntity.ok(
+                ResponseDto.success(isValidCode)
         );
     }
 
