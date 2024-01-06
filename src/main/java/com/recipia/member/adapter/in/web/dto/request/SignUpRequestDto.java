2e94acbf3fbc819df56eb01b58156e4b094067b7
yijoon009 <yijoon009@gmail.com>
2024-01-06 23:59:12 +0900

    [RCP-276] :sparkles: 회원가입 요청 dto에 이미지 파일 필드 추가

--- src/main/java/com/recipia/member/adapter/in/web/dto/request/SignUpRequestDto.java
+++ src/main/java/com/recipia/member/adapter/in/web/dto/request/SignUpRequestDto.java
@@ -5,6 +5,9 @@ import jakarta.validation.constraints.Size;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.ToString;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.util.List;
 
 @ToString
 @Getter
@@ -35,24 +38,10 @@ public class SignUpRequestDto {
     private String address1;        // 주소1
     private String address2;        // 주소2
 
-    @NotBlank
-    @Size(max = 1)
-    private String collectionConsentYn;    // 개인정보 수집 및 이용 동의 여부
-
-    @NotBlank
-    @Size(max = 1)
-    private String marketingConsentYn;    // 마케팅 활용 동의 여부
-
-    @NotBlank
-    @Size(max = 1)
-    private String privacyPolicyConsentYn;    // 개인정보 보호 정책 동의 여부
-
-    @NotBlank
-    @Size(max = 1)
-    private String cookieConsentYn;    // 쿠키 및 추적 기술 사용 동의 여부
+    private MultipartFile profileImage; // 프로필 이미지
 
 
-    private SignUpRequestDto(String email, String password, String fullName, String nickname, String introduction, String telNo, String address1, String address2, String collectionConsentYn, String marketingConsentYn, String privacyPolicyConsentYn, String cookieConsentYn) {
+    private SignUpRequestDto(String email, String password, String fullName, String nickname, String introduction, String telNo, String address1, String address2, MultipartFile profileImage) {
         this.email = email;
         this.password = password;
         this.fullName = fullName;
@@ -61,18 +50,15 @@ public class SignUpRequestDto {
         this.telNo = telNo;
         this.address1 = address1;
         this.address2 = address2;
-        this.collectionConsentYn = collectionConsentYn;
-        this.marketingConsentYn = marketingConsentYn;
-        this.privacyPolicyConsentYn = privacyPolicyConsentYn;
-        this.cookieConsentYn = cookieConsentYn;
+        this.profileImage = profileImage;
     }
 
-    public static SignUpRequestDto of(String email, String password, String fullName, String nickname, String introduction, String telNo, String address1, String address2, String collectionConsentYn, String marketingConsentYn, String privacyPolicyConsentYn, String cookieConsentYn) {
-        return new SignUpRequestDto(email, password, fullName, nickname, introduction, telNo, address1, address2, collectionConsentYn, marketingConsentYn, privacyPolicyConsentYn, cookieConsentYn);
+    public static SignUpRequestDto of(String email, String password, String fullName, String nickname, String introduction, String telNo, String address1, String address2, MultipartFile profileImage) {
+        return new SignUpRequestDto(email, password, fullName, nickname, introduction, telNo, address1, address2, profileImage);
     }
 
-    public static SignUpRequestDto of(String email, String password, String nickname, String introduction) {
-        return new SignUpRequestDto(email, password, null, nickname, introduction, null, null, null, null, null, null, null);
+    public static SignUpRequestDto of(String email, String password, String nickname, String introduction, MultipartFile profileImage) {
+        return new SignUpRequestDto(email, password, null, nickname, introduction, null, null, null, profileImage);
     }
 
 }
