eecce55562242d7752cd198c06b56942142bb76f
yijoon009 <yijoon009@gmail.com>
2024-01-08 04:42:43 +0900

    [RCP-276] :white_check_mark: 회원가입 서비스 임시 커밋

--- src/test/java/com/recipia/member/application/service/SignUpServiceTest.java
+++ src/test/java/com/recipia/member/application/service/SignUpServiceTest.java
@@ -3,46 +3,61 @@ package com.recipia.member.application.service;
 import com.recipia.member.adapter.out.persistence.constant.MemberStatus;
 import com.recipia.member.adapter.out.persistence.constant.RoleType;
 import com.recipia.member.adapter.out.persistenceAdapter.SignUpAdapter;
+import com.recipia.member.application.port.out.port.SignUpPort;
+import com.recipia.member.common.exception.MemberApplicationException;
 import com.recipia.member.domain.Member;
+import com.recipia.member.domain.MemberFile;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
 import org.mockito.junit.jupiter.MockitoExtension;
 import org.springframework.transaction.annotation.Transactional;
+import org.springframework.web.multipart.MultipartFile;
 
 import static org.assertj.core.api.Assertions.assertThat;
-import static org.mockito.ArgumentMatchers.any;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.mockito.ArgumentMatchers.*;
+import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.when;
 
+@Transactional
 @ExtendWith(MockitoExtension.class)
 @DisplayName("[단위] 회원가입 Service 테스트")
 class SignUpServiceTest {
 
     @Mock
-    private SignUpAdapter signUpAdapterMock;
-
+    private SignUpPort signUpPort;
+    @Mock
+    private ImageS3Service imageS3Service;
     @InjectMocks
     private SignUpService sut;
 
-    @DisplayName("[happy] 회원가입에 성공한 회원의 아이디값을 리턴한다.")
-    @Test
-    void signUpTestSuccess() {
-        //given
-        Member member = createMember();
-        //when
-        when(signUpAdapterMock.signUpMember(any())).thenReturn(6L);
-        Long createdMemberId = sut.signUp(member);
-
-        //then
-        assertThat(createdMemberId).isEqualTo(6L);
-        assertThat(member.getPassword()).isNotEqualTo("asdfASDF12#");
+    @BeforeEach
+    void setUp() {
+        MockitoAnnotations.openMocks(this);
     }
 
-    private Member createMember() {
-        return Member.of(null, "test1@example.com", "asdfASDF12#", "Full Name 1", "Nickname1",  MemberStatus.ACTIVE, "Introduction 1", "01012345678",
-                "Address 1-1", "Address 1-2", RoleType.MEMBER);
-    }
+//    @DisplayName("[happy] 프로필 이미지가 없을때 정상적으로 회원가입이 진행된다.")
+//    @Test
+//    void signUp_Success() {
+//        // Given
+//        Member member = Member.of("email@naver.com", "passWord12!@", "fullname", "nickname", MemberStatus.ACTIVE, "intro", "1010", "add1", "add2", RoleType.MEMBER);
+//
+//        MultipartFile profileImage = mock(MultipartFile.class);
+//        when(profileImage.isEmpty()).thenReturn(true);
+//        when(signUpPort.signUpMember(any(Member.class))).thenReturn(1L);
+//
+//        // When
+//        Long result = sut.signUp(member, profileImage);
+//
+//        // Then
+//        assertEquals(1L, result);
+//    }
+
 
 }
