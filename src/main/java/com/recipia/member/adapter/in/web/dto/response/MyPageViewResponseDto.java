a05f37905aea85c87322d50568cb40be806e062c
yijoon009 <yijoon009@gmail.com>
2024-01-11 15:33:41 +0900

    [RCP-293] :sparkles: 마이페이지 조회에 프로필 이미지 필드 추가 (컨트롤러 레이어)

--- src/main/java/com/recipia/member/adapter/in/web/dto/response/MyPageViewResponseDto.java
+++ src/main/java/com/recipia/member/adapter/in/web/dto/response/MyPageViewResponseDto.java
@@ -8,20 +8,22 @@ import lombok.NoArgsConstructor;
 public class MyPageViewResponseDto {
 
     private Long memberId;          // 회원 pk
+    private String profileImageUrl; // 프로필 이미지 url
     private String nickname;        // 닉네임
     private String introduction;    // 한줄소개
     private Long followingCount; // 팔로잉 수
     private Long followerCount;  // 팔로워 수
 
-    private MyPageViewResponseDto(Long memberId, String nickname, String introduction, Long followingCount, Long followerCount) {
+    private MyPageViewResponseDto(Long memberId, String profileImageUrl,  String nickname, String introduction, Long followingCount, Long followerCount) {
         this.memberId = memberId;
+        this.profileImageUrl = profileImageUrl;
         this.nickname = nickname;
         this.introduction = introduction;
         this.followingCount = followingCount;
         this.followerCount = followerCount;
     }
 
-    public static MyPageViewResponseDto of(Long memberId, String nickname, String introduction, Long followingCount, Long followerCount) {
-        return new MyPageViewResponseDto(memberId, nickname, introduction, followingCount, followerCount);
+    public static MyPageViewResponseDto of(Long memberId, String profileImageUrl, String nickname, String introduction, Long followingCount, Long followerCount) {
+        return new MyPageViewResponseDto(memberId, profileImageUrl, nickname, introduction, followingCount, followerCount);
     }
 }
