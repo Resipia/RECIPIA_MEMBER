96b2c1ee289e5793dee05b44eeee3570c1bdf9d1
yijoon009 <yijoon009@gmail.com>
2024-01-11 15:34:03 +0900

    [RCP-293] :sparkles: 마이페이지 조회에 프로필 이미지 필드 추가 (서비스 레이어)

--- src/main/java/com/recipia/member/application/port/in/MyPageUseCase.java
+++ src/main/java/com/recipia/member/application/port/in/MyPageUseCase.java
@@ -3,6 +3,6 @@ package com.recipia.member.application.port.in;
 import com.recipia.member.domain.MyPage;
 
 public interface MyPageUseCase {
-    MyPage viewMyPage(MyPage myPage);
+    MyPage viewMyPage(Long memberId);
     MyPage updateAndViewMyPage(MyPage myPage);
 }
