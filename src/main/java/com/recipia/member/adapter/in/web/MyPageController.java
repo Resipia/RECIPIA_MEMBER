3285584946d812f39438ce8479611c032b6dccd8
yijoon009 <yijoon009@gmail.com>
2024-01-15 02:31:08 +0900

    [RCP-294] :recycle: 마이페이지 조회 로직 수정

--- src/main/java/com/recipia/member/adapter/in/web/MyPageController.java
+++ src/main/java/com/recipia/member/adapter/in/web/MyPageController.java
@@ -14,6 +14,7 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.multipart.MultipartFile;
 
 @RequestMapping("/member/myPage")
 @RequiredArgsConstructor
@@ -24,6 +25,9 @@ public class MyPageController {
     private final MyPageConverter myPageConverter;
     private final SecurityUtils securityUtils;
 
+    /**
+     * 마이페이지 조회 담당
+     */
     @PostMapping("/view")
     public ResponseEntity<ResponseDto<MyPageViewResponseDto>> view() {
         MyPage myPage = myPageUseCase.viewMyPage(securityUtils.getCurrentMemberId());
@@ -32,13 +36,16 @@ public class MyPageController {
         );
     }
 
+    /**
+     * 마이페이지 수정 담당
+     */
     @PostMapping("/update")
-    public ResponseEntity<ResponseDto<MyPageViewResponseDto>> update(@Valid @RequestBody UpdateMyPageRequestDto dto) {
-        MyPage myPage = myPageUseCase.updateAndViewMyPage(myPageConverter.updateRequestDtoToDomain(dto));
+    public ResponseEntity<ResponseDto<Void>> update(@Valid @RequestBody UpdateMyPageRequestDto dto) {
+        MultipartFile profileImage = dto.getProfileImage();
+        myPageUseCase.updateMyPage(myPageConverter.updateRequestDtoToDomain(dto), profileImage);
         return ResponseEntity.ok(
-                ResponseDto.success(myPageConverter.domainToResponseDto(myPage))
+                ResponseDto.success()
         );
-
     }
 
 }
--- src/main/java/com/recipia/member/adapter/out/persistenceAdapter/querydsl/MyPageQueryRepository.java
+++ src/main/java/com/recipia/member/adapter/out/persistenceAdapter/querydsl/MyPageQueryRepository.java
@@ -1,14 +1,18 @@
 package com.recipia.member.adapter.out.persistenceAdapter.querydsl;
 
+import com.querydsl.core.types.ExpressionUtils;
+import com.querydsl.core.types.Projections;
+import com.querydsl.core.types.dsl.Expressions;
 import com.querydsl.jpa.JPAExpressions;
+import com.querydsl.jpa.JPQLQuery;
 import com.querydsl.jpa.impl.JPAQueryFactory;
 import com.recipia.member.domain.MyPage;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Repository;
 
-import static com.querydsl.core.types.Projections.constructor;
 import static com.recipia.member.adapter.out.persistence.QFollowEntity.followEntity;
 import static com.recipia.member.adapter.out.persistence.QMemberEntity.memberEntity;
+import static com.recipia.member.adapter.out.persistence.QMemberFileEntity.memberFileEntity;
 
 @RequiredArgsConstructor
 @Repository
@@ -17,27 +21,45 @@ public class MyPageQueryRepository {
     private final JPAQueryFactory jpaQueryFactory;
 
     /**
-     * [READ] memberId에 해당하는 멤버 정보 조회
+     * [READ] memberId에 해당하는 멤버 정보를 조회한다.
      */
     public MyPage viewMyPage(Long memberId) {
+
+        // 파일 경로 가져오는 서브쿼리
+        JPQLQuery<String> filePathSubQuery = JPAExpressions
+                .select(memberFileEntity.storedFilePath)
+                .from(memberFileEntity)
+                .where(memberFileEntity.memberEntity.id.eq(memberId));
+
+        // 팔로잉 수 가져오는 서브쿼리
+        JPQLQuery<Long> followingCountSubQuery = JPAExpressions
+                .select(Expressions.numberTemplate(Long.class, "coalesce({0}, 0)", followEntity.count()))
+                .from(followEntity)
+                .where(followEntity.followerMember.id.eq(memberId));
+
+        // 팔로워 수 가져오는 서브쿼리
+        JPQLQuery<Long> followerCountSubQuery = JPAExpressions
+                .select(Expressions.numberTemplate(Long.class, "coalesce({0}, 0)", followEntity.count()))
+                .from(followEntity)
+                .where(followEntity.followingMember.id.eq(memberId));
+
+
         return jpaQueryFactory
-                .select(constructor(MyPage.class,
-                        memberEntity.id,
-                        memberEntity.memberFileEntity.storedFilePath.as("profileImageFilePath"),
-                        null,
-                        memberEntity.nickname,
-                        memberEntity.introduction,
-                        JPAExpressions.select(followEntity.count())
-                                .from(followEntity)
-                                .where(followEntity.followerMember.id.eq(memberId)),
-                        JPAExpressions.select(followEntity.count())
-                                .from(followEntity)
-                                .where(followEntity.followingMember.id.eq(memberId))))
+                .select(Projections.fields(MyPage.class,
+                        memberEntity.id.as("memberId"),
+                        ExpressionUtils.as(filePathSubQuery, "profileImageFilePath"),
+                        memberEntity.nickname.as("nickname"),
+                        memberEntity.introduction.as("introduction"),
+                        ExpressionUtils.as(followingCountSubQuery, "followingCount"),
+                        ExpressionUtils.as(followerCountSubQuery, "followerCount")))
                 .from(memberEntity)
                 .where(memberEntity.id.eq(memberId))
                 .fetchOne();
     }
 
+    /**
+     * [UPDATE] 마이페이지에서 기본 정보에 해당하는 항목을 수정한다.
+     */
     public Long updateMyPage(MyPage requestMyPage) {
         return jpaQueryFactory
                 .update(memberEntity)
