01a28f814903433f6fe044b55e8d7dfed2d166fc
yijoon009 <yijoon009@gmail.com>
2024-01-22 16:37:33 +0900

    [RCP-338] :recycle: 한줄소개 한글 100자로 제한

--- src/main/java/com/recipia/member/adapter/in/web/dto/request/UpdateMyPageRequestDto.java
+++ src/main/java/com/recipia/member/adapter/in/web/dto/request/UpdateMyPageRequestDto.java
@@ -1,11 +1,13 @@
 package com.recipia.member.adapter.in.web.dto.request;
 
 import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.ToString;
 import org.springframework.web.multipart.MultipartFile;
+
 
 /**
  * 마이페이지 수정 요청을 담당하는 request dto
@@ -17,6 +19,8 @@ public class UpdateMyPageRequestDto {
 
     @NotBlank
     private String nickname;        // 닉네임
+
+    @Size(max = 300)
     private String introduction;    // 한줄소개
     private MultipartFile profileImage; // 프로필 이미지
     private Integer deleteFileOrder;       // 삭제할 file order
