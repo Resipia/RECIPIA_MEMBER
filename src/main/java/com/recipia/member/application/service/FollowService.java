a6ef1a183f686522fb4d8b5172c598ca622ad4a0
yijoon009 <yijoon009@gmail.com>
2024-01-23 22:08:01 +0900

    [RCP-349] :sparkles: 팔로잉/팔로우 하나의 API로 통합 (service 레이어)

--- src/main/java/com/recipia/member/application/service/FollowService.java
+++ src/main/java/com/recipia/member/application/service/FollowService.java
@@ -2,8 +2,6 @@ package com.recipia.member.application.service;
 
 import com.recipia.member.application.port.in.FollowUseCase;
 import com.recipia.member.application.port.out.port.FollowPort;
-import com.recipia.member.common.exception.ErrorCode;
-import com.recipia.member.common.exception.MemberApplicationException;
 import com.recipia.member.domain.Follow;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
@@ -20,17 +18,22 @@ public class FollowService implements FollowUseCase {
     private final FollowPort followPort;
 
     /**
-     * [CREATE] 팔로우를 생성한다.
+     * [CREATE/DELETE]
+     * 이미 팔로우 관계가 있는 두 회원이면 팔로우 해제 프로세스를 진행하고
+     * 팔로우 관계가 없다면 팔로우 생성 프로세스를 진행한다.
      */
     @Transactional
     @Override
     public Long followRequest(Follow follow) {
-        // 1. 이미 팔로우,팔로잉 관계인지 확인
+        // 이미 팔로우 관계인지 검증
         boolean alreadyFollowing = followPort.existsFollowRelation(follow);
+
         if (alreadyFollowing) {
-            throw new MemberApplicationException(ErrorCode.ALREADY_FOLLOWING);
+            // 팔로우 관계라면 팔로우 해제
+            return followPort.unfollow(follow);
+        } else {
+            // 팔로우 관계가 없다면 팔로우 요청
+            return followPort.follow(follow);
         }
-        // 팔로우 관계가 아니라면 팔로우 생성
-        return followPort.follow(follow);
     }
 }
