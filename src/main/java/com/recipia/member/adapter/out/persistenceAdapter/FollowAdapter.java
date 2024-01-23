f00c95bf3257f755ec717f6459f1111ae3abb8f2
yijoon009 <yijoon009@gmail.com>
2024-01-23 22:08:29 +0900

    [RCP-349] :sparkles: 팔로잉/팔로우 하나의 API로 통합 (db adapter 레이어)

--- src/main/java/com/recipia/member/adapter/out/persistenceAdapter/FollowAdapter.java
+++ src/main/java/com/recipia/member/adapter/out/persistenceAdapter/FollowAdapter.java
@@ -3,6 +3,7 @@ package com.recipia.member.adapter.out.persistenceAdapter;
 import com.recipia.member.adapter.out.persistence.FollowEntity;
 import com.recipia.member.adapter.out.persistence.MemberEntity;
 import com.recipia.member.adapter.out.persistence.constant.MemberStatus;
+import com.recipia.member.adapter.out.persistenceAdapter.querydsl.FollowQueryRepository;
 import com.recipia.member.application.port.out.port.FollowPort;
 import com.recipia.member.common.exception.ErrorCode;
 import com.recipia.member.common.exception.MemberApplicationException;
@@ -18,6 +19,7 @@ public class FollowAdapter implements FollowPort {
 
     private final FollowRepository followRepository;
     private final MemberRepository memberRepository;
+    private final FollowQueryRepository followQueryRepository;
 
     /**
      * [READ] 내가 팔로우 하고 있는 사람인지 검증
@@ -40,6 +42,15 @@ public class FollowAdapter implements FollowPort {
         FollowEntity followEntity = FollowEntity.of(follower, following);
         FollowEntity savedFollow = followRepository.save(followEntity);
         return savedFollow.getId();
+    }
+
+    /**
+     * [DELETE] 팔로우 삭제
+     * 팔로우를 삭제하고 영향받은 데이터 갯수를 반환한다.
+     */
+    @Override
+    public Long unfollow(Follow follow) {
+        return followQueryRepository.deleteFollow(follow);
     }
 
 }
