d406ec176a87b44f36110bc7bc94d4e000ed2ab4
yijoon009 <yijoon009@gmail.com>
2024-01-27 21:35:09 +0900

    [RCP] 다시 /health GET 요청으로 수정

--- src/main/java/com/recipia/member/health/HealthCheckController.java
+++ src/main/java/com/recipia/member/health/HealthCheckController.java
@@ -11,7 +11,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class HealthCheckController {
 
-    @PostMapping("/health")
+    @GetMapping("/health")
     public ResponseEntity<String> healthCheck() {
         return ResponseEntity.ok("Healthy");
     }
